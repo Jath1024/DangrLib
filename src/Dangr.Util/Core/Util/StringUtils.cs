22d0d09e55f4a99af70839662c39a60bc912f813
Dan <dangrv@live.com>
2017-10-14 20:50:12 -0700

    Include binaries in lib folder in nuget packages

--- src/Dangr.Util/Core/Util/StringUtils.cs
+++ src/Dangr.Util/Core/Util/StringUtils.cs
@@ -2,7 +2,7 @@
 //  <copyright file="StringUtils.cs" company="Phoenix Game Studios, LLC">
 //      Copyright (c) 2017 Phoenix Game Studios, LLC. All rights reserved.
 //      Licensed under the MIT License. 
-//      See https://github.com/Dangerdan9631/DangrLib/blob/master/LICENSE for full license information.
+//      See https://github.com/PhoenixGameStudios/DangrLib/blob/master/LICENSE for full license information.
 //  </copyright>
 // -----------------------------------------------------------------------
 namespace Dangr.Core.Util
